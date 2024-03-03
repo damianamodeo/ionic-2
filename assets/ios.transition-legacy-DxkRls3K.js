652e0a314725ccbc9f2a098b4b855a0ff5501adf
damianamodeo <damianamodeotheocratic@gmail.com>
2024-03-04 09:45:01 +1100

    Updates

--- assets/ios.transition-legacy-DxkRls3K.js
+++ assets/ios.transition-legacy-DxkRls3K.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C8uTjgPD.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.o}],execute:function(){
+System.register(["./index-legacy-DoIud_R4.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-R8Oetrva.js
+++ assets/status-tap-legacy-R8Oetrva.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C8uTjgPD.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-DoIud_R4.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */

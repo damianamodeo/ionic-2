3031170d268842f7aab1f47c6b0cbbfb68732f3d
damianamodeo <damianamodeotheocratic@gmail.com>
2024-03-03 17:49:03 +1100

    init

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import legacy from '@vitejs/plugin-legacy'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/ionic-2/",
+  plugins: [
+    react(),
+    legacy()
+  ],
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  //   setupFiles: './src/setupTests.ts',
+  // }
+})
